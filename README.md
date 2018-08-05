1a79a146b37e6cb03821808154cf3ff958bf1f2e
Caio Torre <caiosanches@live.com>
2018-08-05 20:17:05 -0300

    Created new separate parser library

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pixelizer
-Python-based image tool to create pixel art
+# Pixelizer
+Python-based image tool to create pixel art from existing images
+## Usage
+### Command structure
+```bash
+python pixel.py <image> [-w <pixel width>] [-h <pixel height>] [-s <color samples>] [-B]
+```
+### Arguments
+- `-B`: Set big-mode
+    - If big-mode is not set, the output image size is set directly with `-w` and `-h`
+    - If big-mode is set, the output image size is the same as the input image size
+- `-w` and `-h`: Define custom output sizes
+    - If big-mode is not set, the output image size is set as the values passed
+    - If big-mode is set, these arguments define the 'blockiness' of the output (still in beta, possibly leaves invisible column at the left of the image and invisible row at the bottom of the image)
+    - If not passed as arguments, they will be the same as the original image
+- `-s`: Define color sampling (from 1 to 256)
+    - Can be used to simulate older graphics if set to a small enough value
+## Output
+The output image will be saved in the current folder, following the structure:
+```
+[original_image_name]_[output_image_width]x[output_image_height]_[big|small].png
+```
--- pixel.py
+++ pixel.py
@@ -4,37 +4,50 @@ from PIL import Image, ImageDraw
 import sys
 import imageio
 
-image_path = sys.argv[1]
+import argvParser as parser
 
+valid_args = ['-w', '-h', '-s', '-B']
+image_path = sys.argv[1]
+# image_path = parser.getNextValue(sys.argv
+print('Fetching image '+sys.argv[1])
 current_image = imageio.imread(image_path)
 
-pixel_width = int(sys.argv[2])
+# pixel_width = int(sys.argv[2])
+pixel_width = int(parser.getNextValue(sys.argv, '-w', current_image.shape[0]))
+pixel_height = int(parser.getNextValue(sys.argv, '-h', current_image.shape[1]))
 
-color_samples = 4
-color_step = color_samples / 256
+color_samples = int(parser.getNextValue(sys.argv, '-s', 256))
+color_step = color_samples / 256  
 
-if len(sys.argv) > 4:
-    pixel_height = int(sys.argv[3])
-    if sys.argv[4] == '--small':
-        size_out = 1
-        image_out = np.uint8(np.zeros([pixel_width, pixel_height, 3]))
-    elif sys.argv[4] == '--big':
-        size_out = 0
-        image_out = np.uint8(np.zeros(current_image.shape))
-    else:
-        size_out = -1
-        fail_message = sys.argv[4]
+size_out = 1 - int(parser.exists(sys.argv, '-B'))
+
+if size_out == 1:
+    image_out = np.uint8(np.zeros([pixel_width, pixel_height, 4]))
 else:
-    pixel_height = pixel_width
-    if sys.argv[3] == '--small':
-        size_out = 1
-        image_out = np.uint8(np.zeros([pixel_width, pixel_height, 4]))
-    elif sys.argv[3] == '--big':
-        size_out = 0
-        image_out = np.uint8(np.zeros(current_image.shape))
-    else:
-        size_out = -1
-        fail_message = sys.argv[3]
+    image_out = np.uint8(np.zeros(current_image.shape))
+
+# if len(sys.argv) > 4:
+    # pixel_height = int(sys.argv[3])
+    # if sys.argv[4] == '--small':
+        # size_out = 1
+        # image_out = np.uint8(np.zeros([pixel_width, pixel_height, 3]))
+    # elif sys.argv[4] == '--big':
+        # size_out = 0
+        # image_out = np.uint8(np.zeros(current_image.shape))
+    # else:
+        # size_out = -1
+        # fail_message = sys.argv[4]
+# else:
+    # pixel_height = pixel_width
+    # if sys.argv[3] == '--small':
+        # size_out = 1
+        # image_out = np.uint8(np.zeros([pixel_width, pixel_height, 4]))
+    # elif sys.argv[3] == '--big':
+        # size_out = 0
+        # image_out = np.uint8(np.zeros(current_image.shape))
+    # else:
+        # size_out = -1
+        # fail_message = sys.argv[3]
 
 if size_out != -1:
     rec_width_d = int(current_image.shape[0]/(pixel_width))
@@ -60,9 +73,9 @@ if size_out != -1:
                     image_out[cx:cx+rec_width_d,cy:cy+rec_height_d,c] = min(255, math.floor(np.average(current_image[cx:cx+rec_width_d, cy:cy+rec_height_d, c]) * color_step + 0.5) / color_step)
 
     if size_out == 1:
-        imageio.imwrite(image_path[0:-4]+'_'+repr(pixel_width)+'x'+repr(pixel_height)+'_small_flat.png', image_out)
+        imageio.imwrite(image_path[0:-4]+'_'+repr(pixel_width)+'x'+repr(pixel_height)+'_small.png', image_out)
     else:
-        imageio.imwrite(image_path[0:-4]+'_'+repr(pixel_width)+'x'+repr(pixel_height)+'_big_flat.png', image_out)
+        imageio.imwrite(image_path[0:-4]+'_'+repr(pixel_width)+'x'+repr(pixel_height)+'_big.png', image_out)
 
     print("done!")
 else:
